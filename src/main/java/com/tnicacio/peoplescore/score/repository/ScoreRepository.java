55eaf878490c05a411c8a4326595ed79c8df8d44
tnicacio <nicacio.t92@gmail.com>
2022-07-09 16:56:30 -0300

    fix - removing unfinished score repository method

--- src/main/java/com/tnicacio/peoplescore/score/repository/ScoreRepository.java
+++ src/main/java/com/tnicacio/peoplescore/score/repository/ScoreRepository.java
@@ -2,12 +2,9 @@ package com.tnicacio.peoplescore.score.repository;
 
 import com.tnicacio.peoplescore.score.model.ScoreModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScoreRepository extends JpaRepository<ScoreModel, Long> {
 
-    @Query("SELECT s.descricao FROM ScoreModel s WHERE s.inicial <= ")
-    String findDescription(Long score);
 }
